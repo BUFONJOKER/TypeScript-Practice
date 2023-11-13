b84de5dccb0e6b0f4caaefda07d59d208a6eb534
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-11-13 22:58:21 +0500

    DOM manipulation

--- public/app.js
+++ public/app.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('hello world');
+const anchor = document.querySelector('a');
+console.log(anchor.href);
+const form = document.querySelector('.new-item-form');
+console.log(form.children);
+const type = document.getElementById('type');
+const tofrom = document.getElementById('tofrom');
+const details = document.getElementById('details');
+const amount = document.getElementById('amount');
+// const addButtonClick = () =>{
+//     console.log(
+//         type.value,
+//         tofrom.value,
+//         details.value,
+//         amount.value
+//     );
+// }
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-console.log('hello world');
+const anchor = document.querySelector('a')!;
+
+console.log(anchor.href);
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+const type = document.getElementById('type') as HTMLSelectElement;
+const tofrom = document.getElementById('tofrom') as HTMLInputElement;
+const details = document.getElementById('details') as HTMLInputElement;
+const amount = document.getElementById('amount') as HTMLInputElement;
+
+// const addButtonClick = () =>{
+//     console.log(
+//         type.value,
+//         tofrom.value,
+//         details.value,
+//         amount.value
+//     );
+// }
+
+form.addEventListener('submit', (e:Event)=>{
+    e.preventDefault();
+    
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber
+    );
+   
+})
